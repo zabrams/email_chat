f3939e85b6a46a4e4ab038e77fd2c839f9942b69
zabrams <zachary.abrams@gmail.com>
2015-06-10 21:27:27 -0700

    add ability to refresh token

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :google_oauth2, Rails.application.secrets.client_id, Rails.application.secrets.client_secret,
-		{ scope: ['email', 'profile', 'https://www.googleapis.com/auth/gmail.modify'],
-			access_type: ['offline']}
+		{ scope: ['email', 'profile', 'https://www.googleapis.com/auth/gmail.modify'] }
 end
