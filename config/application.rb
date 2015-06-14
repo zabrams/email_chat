8be4a03b7fb481b0547dadf03f0fd3ef25985177
zabrams <zachary.abrams@gmail.com>
2015-06-14 15:40:35 -0700

    integrated ratchet for styling

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module EmailChat
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Add the fonts path
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+    # Precompile additional assets
+    config.assets.precompile += %w( *.svg *.eot *.woff *.ttf )
   end
 end
