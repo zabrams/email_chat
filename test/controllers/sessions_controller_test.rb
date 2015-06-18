4a21a0e1717f86511a18431afcdd83f7fa90ae17
zabrams <zachary.abrams@gmail.com>
2015-06-17 18:44:45 -0700

    remove testing

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should get login path" do
+  	get :new
+  	assert_response :success
+  	assert_select "a", "Authenticate with Google!"
+  	assert_select "header", { count: 0 }
+  end
 end
