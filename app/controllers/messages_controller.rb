a447453b4560f47c365e64b471629c6abd85e062
zabrams <zachary.abrams@gmail.com>
2015-06-19 11:55:16 -0700

    Add helper methods to reduce api calls and build hash by email sender

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,16 +2,16 @@ class MessagesController < ApplicationController
 	before_action :logged_in_user
 
 	def index
-		@gmail = Gmail.new(current_user.fresh_token)
-		@labels = @gmail.labels
-
-		user_inbox = @gmail.inbox
-		@messages = user_inbox['messages']
-		@inbox_size = user_inbox['resultSizeEstimate']
+		connect
+		retrieve_messages
+		group_msg_by_sender
+		#@labels = @gmail.labels
 	end
 
 	def inbox
 	end
+
+
 
 	private
 		def logged_in_user
@@ -21,5 +21,51 @@ class MessagesController < ApplicationController
 			end
 		end
 
+		def connect
+			if @gmail == nil
+				@gmail = Gmail.new(current_user.fresh_token)
+			end 
+		end
+
+		def retrieve_messages
+			if @details == nil
+				@details = { }
+				messages = @gmail.inbox['messages']
+				messages.each do |msg| 
+					@details.merge!(msg['id'] =>  @gmail.get_details(msg['id'])) 
+				end
+			end 
+		end
+		
+		#FIGURE OUT HOW TO GET DETAILS OF MESSAGES ONLY ONE TIME
+		#Take all messages - iterate through for unique emails - 
+		#then record the message IDs for all messages tied to that email
+		def group_msg_by_sender 
+			#take @details and sort into has with email at the key
+			@sender_hash = {}
+			@details.each do |id, data|
+				name = data[:sender]
+				if @sender_hash.blank?
+					@sender_hash.merge!( name => { data[:date] => data } )
+				else
+					exist = false
+					@sender_hash.each do |sender, email|
+						if name == sender
+							exist = true
+						else
+							exist = false
+						end 
+					end
+
+					if exist
+						@sender_hash[name].merge!( data[:date] => data )
+					else
+						@sender_hash.merge!( name => { data[:date] => data } )
+					end
+				end
+			end
+		end
+
+		#Create Similar variable for retrieving messages
 		
 end
--- app/helpers/gmail.rb
+++ app/helpers/gmail.rb
@@ -31,7 +31,19 @@ class Gmail
   		data = JSON.parse(results.body)
   		{ subject: get_gmail_attribute(data, 'Subject'),
     	from: get_gmail_attribute(data, 'From'),
-    	body: get_gmail_body(data), data: data }
+    	body: get_gmail_body(data), 
+      sender: get_gmail_attribute(data, 'Return-Path'),
+      date:  get_gmail_attribute(data, 'Date'),
+      #data: data
+      }
+  end
+
+  def get_messages_from(sender)
+      results = @client.execute!(
+        :api_method => @service.users.messages.list,
+        :parameters => {'userId' => 'me', 'q' => "from:#{sender}" },
+        :headers => {'Content-Type' => 'application/json'})
+      JSON.parse(results.body)
   end
 
   def get_gmail_attribute(gmail_data, attribute)
